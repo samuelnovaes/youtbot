05239e7a8e03227c2ab9ac9513dab85cdcec03e0
Samuel Novaes <samuel.novaes.96@gmail.com>
2019-06-29 23:40:03 -0300

    update

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV != 'development') {
 			next()
 		}
 		else {
-			res.redirect(`https://${req.hostname}/${req.originalUrl}`)
+			res.redirect(`https://${req.headers.host}/${req.url}`)
 		}
 	})
 }
