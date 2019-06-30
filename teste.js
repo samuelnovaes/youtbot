3760970118ebc0cdc0ba3375022e1a1b47925bf6
Samuel Novaes <samuel.novaes.96@gmail.com>
2019-06-30 01:16:23 -0300

    update

--- teste.js
+++
@@ -1,8 +0,0 @@
-const msg = process.argv[2]
-const reg = /^search (.+) (\d+)$/i
-
-if (reg.test(msg)) {
-	const [, query, page] = msg.trim().match(reg)
-	console.log(query.trim())
-	console.log(page)
-}
